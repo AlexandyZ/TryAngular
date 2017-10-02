cb92e1b4bf3682a64db361c2fcede36f104d4c72
AlexandyZ <zyf19861014@gmail.com>
2017-10-02 00:47:03 -0700

    add routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,8 +7,17 @@ import { Hero } from './hero';
 export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
-    /*return new Promise(resolve => {
-      setTimeout(() => resolve(this.getHeroes()), 2000);
-    });*/
   }
+
+  getHero(id: number): Promise<Hero> {
+   return this.getHeroes()
+              .then(heroes => heroes.find(hero => hero.id === id));
+  }
+
+  /*getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }*/
 }
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: [ './heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor (
+    private heroService: HeroService,
+    private router: Router
+  ) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+  ngOnInit(): void{
+    this.getHeroes();
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
